7b2b813a70687eab99d7220aeae6519ef6d01d9f
Chris Earnhardt <chris@agilereasoning.com>
2011-06-27 11:29:09 -0400

    Add mothod to populate drop-down state values.

--- app/models/state.rb
+++ app/models/state.rb
@@ -2,4 +2,10 @@ class State < ActiveRecord::Base
   validates_uniqueness_of :code, :name, :within => :country_code, :case_sensitive => false
   validates_presence_of :code, :name
   validates_inclusion_of :country_code, :in => %w{CA US MX}
+
+  class << self
+    def to_options
+      State.find(:all, :conditions => {:country_code => 'US'}, :order => :code).collect {|s| [s.code + ' - ' + s.name, s.id]}
+    end
+  end
 end
--- spec/models/state_spec.rb
+++ spec/models/state_spec.rb
@@ -1,5 +1,21 @@
 require 'spec_helper'
 
 describe State do
-  true
+  fixtures :states
+
+  describe '.to_options' do
+    it 'finds all of the states and return their code/name and id in pairs' do
+      State.to_options.first(9).should == [
+        ['AA - Armed Forces Americas', states(:states_73).id],
+        ['AE - Armed Forces', states(:states_74).id],
+        ['AK - Alaska', states(:states_15).id],
+        ['AL - Alabama', states(:states_14).id],
+        ['AP - Armed Forces Pacific', states(:states_75).id],
+        ['AR - Arkansas', states(:states_18).id],
+        ['AS - American Samoa', states(:states_16).id],
+        ['AZ - Arizona', states(:states_17).id],
+        ['CA - California', states(:states_19).id]
+      ]
+    end
+  end
 end
