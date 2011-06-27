ee0dec7431556b3a618f8373b0045b474aec47fc
U-FSSA\cearnhardt <cearnhardt@3VZ3NN1.state.in.us>
2011-06-27 08:28:29 -0400

    Default rails install

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CsvTest::Application.config.secret_token = '2b8c7a71ab76d9a1a5b427dbaf9c6910de7386c2ea3b228f7af725dffe52c1a1622a7eb2cdc5d8560236742b6123c8e274d7a84dceb20618c9548365e6d3e929'
