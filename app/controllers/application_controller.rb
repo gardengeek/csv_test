d4db786956091287bcec192e9a2ff3d32001dba7
U-FSSA\cearnhardt <cearnhardt@3VZ3NN1.state.in.us>
2011-06-27 08:36:43 -0400

    Add features, controllers, etc.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,32 @@
 class ApplicationController < ActionController::Base
+  include Clearance::Authentication
+  include ::SslRequirement
   protect_from_forgery
+
+  private
+
+  def authorize
+    if signed_in?
+      Time.zone = current_user.time_zone
+    else
+      deny_access "You must be signed in to access this page"
+    end
+  end
+
+  def signed_in?
+    current_user && current_user.active? && current_user.confirmed?
+  end
+
+  def require_no_user
+    if signed_in?
+      flash.keep
+      redirect_to account_url
+      return false
+    end
+  end
+  
+  def ssl_required?
+    return super if Rails.env.production?
+    false
+  end
 end
