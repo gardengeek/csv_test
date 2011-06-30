92fce0b435ccb12e93f54e1ad8ce18665e8f59a3
Chris Earnhardt <chris@agilereasoning.com>
2011-06-30 15:00:09 -0400

    Add age methods to person.

--- app/models/person.rb
+++ app/models/person.rb
@@ -11,4 +11,18 @@ class Person < ActiveRecord::Base
   def birth_date_str
     self[:birth_date].to_s
   end
+
+  def age
+    return('') if self.birth_date.nil?
+    age = Date.today.year - self.birth_date.year
+    age -= 1 if Date.today < (self.birth_date + age.years)
+    age
+  end
+
+  def age_at_create
+    return('') if self.birth_date.nil?
+    age = self.created_at.year - self.birth_date.year
+    age -= 1 if self.created_at < (self.birth_date + age.years)
+    age
+  end
 end
--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -1,4 +1,43 @@
-require 'spec_helper'
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe Person do
+
+  describe '#age' do
+    person = Person.new
+    { Date.today - 1.year => 1,
+      Date.today - 1.year - 1.day => 1,
+      Date.today - 1.year + 1.day => 0,
+      Date.today - 3.year => 3,
+      Date.today - 3.year - 1.day => 3,
+      Date.today - 3.year + 1.day => 2,
+      '' => ''
+    }.each do |birth_date, age|
+      context "with birth date of #{birth_date}" do
+        it "calculates the age of #{age}" do
+          person.birth_date = birth_date
+          person.age.should == age
+        end
+      end
+    end
+  end
+
+  describe '#age_at_created' do
+    person = Person.new
+    { [Date.parse('1970-02-23'), Date.parse('2001-02-23 12:00:00')] => 31,
+      [Date.parse('1970-02-23'), Date.parse('2001-02-22 12:00:00')] => 30,
+      [Date.parse('1970-02-23'), Date.parse('2001-02-24 12:00:00')] => 31,
+      [Date.parse('1972-02-29'), Date.parse('2001-02-28 12:00:00')] => 29,
+      [Date.parse('1972-02-29'), Date.parse('2001-03-01 12:00:00')] => 29,
+      [Date.parse('1972-02-29'), Date.parse('2001-02-27 12:00:00')] => 28,
+      '' => ''
+    }.each do |(birth_date, created_at), age|
+      context "with birth date of #{birth_date}" do
+        it "calculates the age of #{age}" do
+          person.birth_date = birth_date
+          person.created_at = created_at
+          person.age_at_create.should == age
+        end
+      end
+    end
+  end
 end
